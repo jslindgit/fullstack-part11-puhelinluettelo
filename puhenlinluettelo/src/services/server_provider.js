da93465fb23a9bff05ec3740a1f81811c32334ed
Joonas Lindstedt <68861164+jslindgit@users.noreply.github.com>
2023-06-17 11:18:26 +0300

    11.20

--- puhenlinluettelo/src/services/server_provider.js
+++ puhenlinluettelo/src/services/server_provider.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/persons'
-//const baseUrl = '/api/persons'
+//const baseUrl = 'http://localhost:3001/api/persons' // <- local
+const baseUrl = '/api/persons' // <- fly.io
 
 const deleteItem = (id) => {
   const request = axios.delete(`${baseUrl}/${id}`)
