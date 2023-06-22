3fee9d62fd9734998436b59bdca7f226eeaa1066
Joonas Lindstedt <68861164+jslindgit@users.noreply.github.com>
2023-06-23 00:40:09 +0300

    vmp

--- puhelinluettelo/src/services/server_provider.js
+++ puhelinluettelo/src/services/server_provider.js
@@ -3,23 +3,23 @@ const baseUrl = 'http://localhost:3001/api/persons' // <- local
 //const baseUrl = '/api/persons' // <- fly.io
 
 const deleteItem = (id) => {
-	const request = axios.delete(`${baseUrl}/${id}`)
-	return request.then((response) => response.data)
+    const request = axios.delete(`${baseUrl}/${id}`)
+    return request.then((response) => response.data)
 }
 
 const getAll = () => {
-	const request = axios.get(baseUrl)
-	return request.then((response) => response.data)
+    const request = axios.get(baseUrl)
+    return request.then((response) => response.data)
 }
 
 const postNew = (nameObj) => {
-	const request = axios.post(baseUrl, nameObj)
-	return request.then((response) => response.data)
+    const request = axios.post(baseUrl, nameObj)
+    return request.then((response) => response.data)
 }
 
 const update = (id, newObj) => {
-	const request = axios.put(`${baseUrl}/${id}`, newObj)
-	return request.then((response) => response.data)
+    const request = axios.put(`${baseUrl}/${id}`, newObj)
+    return request.then((response) => response.data)
 }
 
 export default { deleteItem, getAll, postNew, update }
