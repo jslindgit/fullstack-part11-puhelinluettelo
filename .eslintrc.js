3aa5c17bf8e4ec095341306649bca46c97db75f5
Joonas Lindstedt <68861164+jslindgit@users.noreply.github.com>
2023-06-22 21:31:03 +0300

    e2e

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,22 @@
 module.exports = {
-	env: {
-		node: true,
-		commonjs: true,
-		es2021: true,
-	},
-	extends: 'eslint:recommended',
-	overrides: [],
-	parserOptions: {
-		ecmaVersion: 'latest',
-	},
-	rules: {
-		indent: ['error', 'tab'],
-		quotes: ['error', 'single'],
-		semi: ['error', 'never'],
-		eqeqeq: 'error',
-		'no-trailing-spaces': 'error',
-		'object-curly-spacing': ['error', 'always'],
-		'arrow-spacing': ['error', { before: true, after: true }],
-		'no-console': 0,
-	},
+    env: {
+        node: true,
+        commonjs: true,
+        es2021: true,
+    },
+    extends: 'eslint:recommended',
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 'latest',
+    },
+    rules: {
+        indent: ['error', 4],
+        quotes: ['error', 'single'],
+        semi: ['error', 'never'],
+        eqeqeq: 'error',
+        'no-trailing-spaces': 'error',
+        'object-curly-spacing': ['error', 'always'],
+        'arrow-spacing': ['error', { before: true, after: true }],
+        'no-console': 0,
+    },
 }
